4e510c3d461c551248d51fc6385482e7345b33a0
springkyu <seika.k.r0610@gmail.com>
2021-08-03 08:52:08 +0000

    [アソシエーション]応用課題４

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :followers
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, through: :relationships, source: :followed
 end
