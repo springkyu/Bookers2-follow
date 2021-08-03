2a10237d2fa3be942b2c246d21306ac428106a25
springkyu <seika.k.r0610@gmail.com>
2021-08-03 09:16:10 +0000

    [Userモデルにメソッド記述]応用課題４

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,15 @@ class User < ApplicationRecord
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, through: :relationships, source: :followed
+
+
+  def follow(user_id)
+    ralationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id)
+  end
+  def following?(user)
+    following.include?(user)
+  end
 end
