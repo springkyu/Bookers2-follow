e817cfce77fbeb0fda4cea6825e82d39f5c614c2
springkyu <seika.k.r0610@gmail.com>
2021-08-03 05:49:29 +0000

    users-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_053915) do
+ActiveRecord::Schema.define(version: 2021_08_03_054405) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_03_053915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.text "intoduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
