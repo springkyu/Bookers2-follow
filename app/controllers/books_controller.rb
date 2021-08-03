0c140eebd3b589961b18e28cbefed3a53ac199a2
springkyu <seika.k.r0610@gmail.com>
2021-08-03 05:07:42 +0000

    books-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,30 @@
 class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice]="You have creatad book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
 end
